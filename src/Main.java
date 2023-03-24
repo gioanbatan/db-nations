26a51b182d3352cc89a0d6507c0c4adb241381e5
gioanbatan <giovannibassani@hotmail.com>
2023-03-24 16:52:39 +0100

    Fix: nationQuery

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,31 @@ public class Main {
         String USER = "root";
         String PASSWORD = "root";
 
+        // Queries
+        String nationsQuery = """
+                select c.name as country, c.country_id, r.name as region, cont.name as continent
+                from countries c
+                join regions r on r.region_id  = c.region_id
+                join continents cont on cont.continent_id = r.continent_id
+                where c.name like ?
+                order by c.name;
+                """;
+
+        String countryLanguagesQuery = """
+                select c.name  country, language
+                from languages l
+                join country_languages cl on l.language_id  = cl.language_id
+                join countries c on c.country_id = cl.country_id
+                where cl.country_id = ?;
+                """;
+
+        String countryStatsQuery = """
+                select c.name country, 'year', population, gdp
+                from country_stats cs
+                join countries c on c.country_id = cs.country_id
+                where cs.country_id = ?;
+                """;
+
         Scanner scan = new Scanner(System.in);
 
         System.out.println("-- SQL query in java --");
@@ -20,26 +45,47 @@ public class Main {
         // Connection object
         try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
 
-            // SQL query
-            String query = """
-                    select c.name as country, c.country_id, r.name as region, cont.name as continent
-                    from countries c
-                    join regions r on r.region_id  = c.region_id
-                    join continents cont on cont.continent_id = r.continent_id
-                    where c.name like ?
-                    order by c.name;
-                    """;
             // Prepared statement from query
-            try (PreparedStatement ps = connection.prepareStatement(query)) {
+            try (PreparedStatement ps = connection.prepareStatement(nationsQuery)) {
                 // Parameters
-                userInput = "%" + userInput + "%";
-                ps.setString(1, userInput);
+                ps.setString(1, "%" + userInput + "%");
 
                 // Execute query
                 try (ResultSet rs = ps.executeQuery()) {
                     // Result iterations
                     while (rs.next()) {
                         String nationName = rs.getString(1);
+                        int nationId = rs.getInt(2);
+                        String regionName = rs.getString(3);
+                        String continentName = rs.getString(4);
+                        System.out.printf("%-47s %4d %29s %16s%n", nationName, nationId, regionName, continentName);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        // Second user input
+        System.out.print("-> Inserire l'id di una nazione: ");
+        userInput = scan.nextLine();  // ADD handle exceptions
+
+        // Connection object
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
+
+            // Prepared statement from query
+            try (PreparedStatement ps = connection.prepareStatement(countryLanguagesQuery)) {
+                // Parameters
+                ps.setInt(1, Integer.parseInt(userInput));
+
+                // Execute query
+                try (ResultSet rs = ps.executeQuery()) {
+                    String nationName = rs.getString(1);
+                    System.out.println("Nazione selezionata: " + nationName);
+
+                    /*List <String> */
+                    // Result iterations
+                    while (rs.next()) {
                         int nationId = rs.getInt(2);
                         String regionName = rs.getString(3);
                         String continentName = rs.getString(4);
